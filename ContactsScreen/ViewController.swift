ca3ad310f2b92e847906f927a57b540c2e4bcdc0
serhatyaroglu <61588155+serhatyaroglu@users.noreply.github.com>
2020-10-06 09:57:03 +0300

    Initial Commit

---
+++ ContactsScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsScreen
+//
+//  Created by serhat yaroglu on 6.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
