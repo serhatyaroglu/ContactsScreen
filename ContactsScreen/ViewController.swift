d6fb12fa02b6927e4b2ab31ee94bcd62b2fcf114
serhatyaroglu <61588155+serhatyaroglu@users.noreply.github.com>
2020-10-08 16:52:28 +0300

    dsaffd
    dfg

--- ContactsScreen/ViewController.swift
+++ ContactsScreen/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ButtonRadius: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ButtonRadius.layer.cornerRadius = 12
     }
 
 
